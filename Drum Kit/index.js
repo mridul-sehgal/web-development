b34fffeae4a349a3dc30c822a947c4bcb60a1d37
Mridul Sehgal <105665765+mridul-sehgal@users.noreply.github.com>
2023-05-31 16:32:14 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,65 @@
+var butLen = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < butLen; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var butInnerHtml = this.innerHTML;
+    makeSound(butInnerHtml);
+    buttonAnimation(butInnerHtml);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function buttonAnimation(key1) {
+  var btn = document.querySelector("." + key1);
+  btn.classList.add("pressed");
+  setTimeout(function () {
+    btn.classList.remove("pressed");
+  }, 1000);
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      alert(butInnerHtml);
+      break;
+  }
+}
